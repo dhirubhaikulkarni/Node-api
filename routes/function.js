caf4d4c7fbda4ec018bf1f6c85b46b4bb9244fb4
Dhirubhai kulkarni <kulkarnidhirubhai@gmail.com>
2022-03-18 17:53:39 +0530

    Category Related Operation (Only Add)

--- routes/function.js
+++ routes/function.js
@@ -26,58 +26,66 @@ module.exports = (app) => {
       });
   });
 
-
-  app.get('/api/BooksDetails/:bookId', (req, res) => {
-    bookdetails.findOne({ BDID: req.params.bookId })
-    .populate('Category')
-    .populate('Publisher')
-    .exec((err, result) => {
-        return res.send(result);
-    });
-  });
-
-  app.post('/api/BooksDetails/edit', (req, res)=>{
-    categorydetails.findOne({Name: req.body.data.Category.Name}, (err, found1) => {
-      publisherdetails.findOne({Name: req.body.data.Publisher.Name}, (err, found2) => {
-        bookdetails.findOne({ BDID: req.body.id }, (err, found3) => {
-          if(found3) {
-            found3.bookname = req.body.data.bookname;
-            found3.Category = found1._id;
-            found3.Publisher = found2._id;
-            found3.quantity = req.body.data.quantity;
-            return found3.save((err) => {
-              return res.send({status: 'OK'});
-            })
+  app.post("/api/BooksDetails/edit", (req, res) => {
+    categorydetails.findOne(
+      { Name: req.body.data.Category.Name },
+      (err, found1) => {
+        publisherdetails.findOne(
+          { Name: req.body.data.Publisher.Name },
+          (err, found2) => {
+            bookdetails.findOne({ BDID: req.body.id }, (err, found3) => {
+              if (found3) {
+                found3.bookname = req.body.data.bookname;
+                found3.Category = found1._id;
+                found3.Publisher = found2._id;
+                found3.quantity = req.body.data.quantity;
+                return found3.save((err) => {
+                  return res.send({ editstatus: "Book Edited Successfully" });
+                });
+              }
+            });
           }
+        );
+      }
+    );
+  });
+  
+  app.post("/api/CategoryDetails/edit", (req, res) => {
+    categorydetails.findOne({ categoryid: req.body.id }, (err, found1) => {
+      if (found1) {
+        found1.Name = req.body.data.Name;
+        return found1.save((err) => {
+          return res.send({ status: "Category Edit Successfully" });
         });
-      });
+      }
     });
   });
 
-  app.post('/api/BooksDetails/add', (req, res)=>{
-    categorydetails.findOne({Name: req.body.data.Category.Name}, (err, found1) => {
-      publisherdetails.findOne({Name: req.body.data.Publisher.Name}, (err, found2) => {
-        bookdetails.findOne({ BDID: req.body.data.BDID }, (err, found3) => {
-          if(!found3) {
-            const newData = new bookdetails();
-            newData.BDID = req.body.data.BDID;
-            newData.bookname = req.body.data.bookname;
-            newData.Category = found1._id;
-            newData.Publisher = found2._id;
-            newData.quantity = req.body.data.quantity;
-            return newData.save((err) => {
-              return res.send({status: 'OK'});
-            })
+  app.post("/api/BooksDetails/add", (req, res) => {
+    categorydetails.findOne(
+      { Name: req.body.data.Category.Name },
+      (err, found1) => {
+        publisherdetails.findOne(
+          { Name: req.body.data.Publisher.Name },
+          (err, found2) => {
+            bookdetails.findOne({ BDID: req.body.data.BDID }, (err, found3) => {
+              if (!found3) {
+                const newData = new bookdetails();
+                newData.BDID = req.body.data.BDID;
+                newData.bookname = req.body.data.bookname;
+                newData.Category = found1._id;
+                newData.Publisher = found2._id;
+                newData.quantity = req.body.data.quantity;
+                return newData.save((err) => {
+                  return res.send({ status: "Book Added Successfully" });
+                });
+              }
+            });
           }
-        });
-      });
-    });
+        );
+      }
+    );
   });
-
-
- 
-
-
 
   app.get("/api/BooksDetails/delete/:BookId", (req, res) => {
     bookdetails.findOne(
@@ -98,10 +106,36 @@ module.exports = (app) => {
     );
   });
 
-  app.get("/api/categorydetails", (req, res) => {
+  app.get("/api/CategoryDetails", (req, res) => {
     categorydetails.find().exec((err, categorydetails) => {
       return res.send(categorydetails);
     });
+  });
+
+  app.get("/api/CategoryDetails/:categoryId", (req, res) => {
+    categorydetails
+      .findone({ categoryid: req.params.categoryid })
+      .exec((err, categorydetails) => {
+        return res.send(categorydetails);
+      });
+  });
+
+
+  app.post("/api/CategoryDetails/add", (req, res) => {
+    categorydetails.findOne(
+      { categoryid: req.body.data.categoryid },
+      (err, found1) => {
+        if (!found1) {
+          const newData = new categorydetails();
+          newData.categoryid = req.body.data.categoryid;
+          newData.Name = req.body.data.Name;
+
+          return newData.save((err) => {
+            return res.send({ status: "Category Add Successfully" });
+          });
+        }
+      }
+    );
   });
 
   app.get("/api/publisherdetails", (req, res) => {
